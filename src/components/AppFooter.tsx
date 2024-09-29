c1663fb2922b6491c31f518b12e25f074e59a3c5
gitong23 <gitong23@gmail.com>
2024-09-29 20:40:59 +0700

    initail commit

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppFooter = () => {
+  return (
+    <>
+      <p>created by codingthailand.com</p>
+      <p>Hello Welcome</p>
+    </>
+  );
+};
+
+export default AppFooter;
